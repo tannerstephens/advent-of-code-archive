69a53eae00e504ebc3c30f0e2385726ba7404ac7
Tanner Stephens <tannerrstephens@gmail.com>
2021-12-01 09:34:11 -0600

    2021 Day 1 cleanup

--- 2021/day1/run.py
+++ 2021/day1/run.py
@@ -9,30 +9,27 @@ with open(f'{dir_path}/input') as f:
 def parse_input():
   return [int(line) for line in puzzle_input]
 
+def count_window(puzzle_input: list[int], comparison_size: int = 1) -> int:
+  c = 0
+
+  n = sum(puzzle_input[:comparison_size])
+
+  for i in range(len(puzzle_input) - comparison_size):
+    s = sum(puzzle_input[i+1:i+comparison_size+1])
+    c += s > n
+    n = s
+
+  return c
+
 def part1():
   pi = parse_input()
 
-  c = 0
-  l = 99999
-
-  for n in pi:
-    c += n > l
-    l = n
-
-  return c
+  return count_window(pi)
 
 def part2():
   pi = parse_input()
 
-  c = 0
-  l = 9999999
-
-  for i, n1 in enumerate(pi[:-2]):
-    n = n1 + pi[i+1] + pi[i+2]
-    c += n > l
-    l = n
-
-  return c
+  return count_window(pi, 3)
 
 def main():
   part1_res = part1()
