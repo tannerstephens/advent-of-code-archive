907bcb8505fc71d7b36282b9671c3ba1eaa575f1
Tanner Stephens <tstephens@mysidewalk.com>
2021-12-06 10:35:12 -0600

    Xfer

--- 2021/day5/run.py
+++ 2021/day5/run.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 from os.path import dirname, realpath
+from re import compile
+
 dir_path = dirname(realpath(__file__))
 
 with open(f'{dir_path}/input') as f:
   puzzle_input = f.read().split('\n')[:-1]
 
 def parse_input():
-  return [line for line in puzzle_input]
+  regex = compile(r'(\d+),(\d+) -> (\d+),(\d+)')
+
+  return [[int(n) for n in regex.search(line).groups()] for line in puzzle_input]
+
+
 
 def part1():
   pi = parse_input()
+
+
 
 def part2():
   pi = parse_input()
